0c2f6bba569891b129e0ddd9fd2d4dcd65a19e27
Ganjar Hadiatna <ganjardbc@gmail.com>
2021-06-30 14:41:23 +0700

    Create dots component

---
+++ src/modules/Dots.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Dots extends Component {
+    render () {
+        return (
+            <div className="fa fa-1x fa-circle" style={{ position: 'relative', top: -2, fontSize: 3, color: '999', marginRight: 2, marginLeft: 2}}></div>
+        )
+    }
+}
+
+export default Dots 
