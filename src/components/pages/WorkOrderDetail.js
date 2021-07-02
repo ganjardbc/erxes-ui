aa402454b7062574f973216b2038fac6ec846489
riandannyraka <riandannyraka@gmail.com>
2021-07-02 14:48:35 +0700

    Remove warnings

--- src/components/pages/WorkOrderDetail.js
+++ src/components/pages/WorkOrderDetail.js
@@ -1,108 +1,4 @@
 import React, { Component } from 'react'
-import TabBar from '../../modules/TabBar'
-import Dots from '../../modules/Dots'
-
-class CardHeader extends Component {
-    render() {
-        return (
-            <div className="padding-15px display-flex space-between">
-                <div className="width width-15">
-                    <div className="txt-site txt-10 txt-main txt-bold">Vehicle</div>
-                </div>
-                <div className="width width-10">
-                    <div className="txt-site txt-10 txt-main txt-bold">Number</div>
-                </div>
-                <div className="width width-30">
-                    <div className="txt-site txt-10 txt-main txt-bold">Details</div>
-                </div>
-                <div className="width width-10">
-                    <div className="txt-site txt-10 txt-main txt-bold">Assigned</div>
-                </div>
-                <div className="width width-5">
-                    <div className="txt-site txt-10 txt-main txt-bold">Total</div>
-                </div>
-                <div className="width width-10">
-                    <div className="txt-site txt-10 txt-main txt-bold"></div>
-                </div>
-            </div>
-        )
-    }
-}
-
-class CardVehicle extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {}
-    }
-
-    render() {
-        return (
-            <div className="padding-15px display-flex space-between">
-                <div className="width width-15">
-                    <div className="display-flex">
-                        <div style={{ width: 40, marginRight: 15 }}>
-                            <div className="image image-40px image-radius background-blue" style={{ overflow: 'hidden' }}>
-                                <img style={{ height: '100%', width: 'auto' }} src="https://asset.kompas.com/crops/O28JjUYAX1rzviozhepsrpdovk0=/79x29:1226x794/750x500/data/photo/2019/03/20/2754664179.jpeg" alt="" />
-                            </div>
-                        </div>
-                        <div style={{ width: 'calc(100% - 55px)' }}>
-                            <div className="txt-site txt-12 txt-main txt-bold" style={{ marginBottom: 5 }}>Chevy Service Van</div>
-                            <div className="display-flex">
-                                <div className="fa fa-1x fa-circle" style={{ fontSize: 11, color: 'red', marginTop: 2, marginRight: 5 }}></div>
-                                <div className="txt-site txt-9 txt-primary">Out of Service <Dots /> Van <Dots /> Admin</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="width width-10">
-                    <div className="txt-site txt-10 txt-main txt-bold" style={{ marginBottom: 3 }}>#285</div>
-                    <div className="display-flex">
-                        <div className="fa fa-1x fa-circle" style={{ fontSize: 11, color: 'teal', marginTop: 2, marginRight: 5 }}></div>
-                        <div className="txt-site txt-9 txt-primary">1 hr 59 min</div>
-                    </div>
-                </div>
-                <div className="width width-30">
-                    <div className="display-flex margin-bottom-5px">
-                        <div className="fa fa-1x fa-wrench" style={{ fontSize: 12, color: '#000', marginTop: 2, marginRight: 5 }}></div>
-                        <div className="txt-site txt-10 txt-main txt-bold">Tire Replacement</div>
-                    </div>
-                    <div className="display-flex margin-bottom-5px">
-                        <div className="fa fa-1x fa-info-circle" style={{ fontSize: 12, color: '#000', marginTop: 2, marginRight: 5 }}></div>
-                        <div style={{ cursor: 'pointer' }} className="txt-site txt-10 txt-main txt-blue txt-bold">Breakdown</div>
-                    </div>
-                    <div className="display-flex margin-bottom-5px">
-                        <button type="button" className="btn btn-small btn-black">Accident</button>
-                    </div>
-                    <div className="display-flex margin-bottom-5px">
-                        <div className="txt-site txt-9 txt-primary">Issued on <label className="txt-site txt-9 txt-primary txt-underline">Nov 7, 2019</label></div>
-                    </div>
-                </div>
-                <div className="width width-10">
-
-                </div>
-                <div className="width width-5">
-                    <div className="txt-site txt-10 txt-main txt-extra-bold">{`$443.96`}</div>
-                </div>
-                <div className="width width-10">
-                    <div className="display-flex center">
-                        <button className="btn btn-small btn-sekunder margin-right-5px" style={{ height: 30 }}>
-                            <i className="fa fa-lw fa-eye" />
-                        </button>
-                        <button className="btn btn-small btn-sekunder margin-right-5px" style={{ width: 60, height: 30 }}>
-                            <div className="display-flex">
-                                <i className="fa fa-lw fa-flag" style={{ marginRight: 5 }} />
-                                <i className="fa fa-xs fa-chevron-down" />
-                            </div>
-                        </button>
-                        <button className="btn btn-small btn-grey margin-right-5px" style={{ width: 30, height: 30 }}>
-                            <i className="fa fa-lw fa-ellipsis-v" />
-                        </button>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-}
 
 class WorkOrderDetail extends Component {
     constructor(props) {
@@ -128,7 +24,6 @@ class WorkOrderDetail extends Component {
     }
 
     render() {
-        const { navigator } = this.state
         return (
             <div className="main-content no-padding">
                 <div className="padding-15px display-flex space-between background-white border-bottom">
